ee41592f814ca5850c268aaee20fd86292ab992b
Paulo Lemes <paulo-lemes@live.com>
2024-04-26 15:46:00 -0300

    ProductCard component tests

---
+++ src/util.ts
@@ -0,0 +1,10 @@
+export const defaultProduct = {
+  id: 1,
+  name: "Name",
+  brand: "Brand",
+  description: "Description",
+  photo: "https://imagedomain.ccom",
+  price: "10.00",
+  createdAt: "2000-01-01T00:00:00.000Z",
+  updatedAt: "2000-01-01T00:00:00.000Z",
+};
